addf9c0485ebe20b4d09f99f199ee03fc2da22e4
SergeOg <gosha_006@mail.ru>
2021-05-21 21:32:07 +0300

    Реализация Task_2,3,4

---
+++ src/main/java/ru/geekbrains/lesson1/MainActivity2.java
@@ -0,0 +1,21 @@
+package ru.geekbrains.lesson1;
+//2. Создать ещё один макет (если не получается, то проект) с несколькими EditText, где
+//использовать атрибут inputType: text, textPersonName, phone, number, textPassword,
+//textEmailAddress и другие значения.
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
+//3. Добавить в проект элемент CalendarView.
+//4. * Разобраться, что такое параметр ems:
+//Данный атрибут используется для определения значения ширины TextView в единицах измерения EM.
+//При этом EM это единица измерения в Typography (Типография), его значением является ширина буквы "M".
+//Таким образом android:ems="5" - значит настроить ширину для TextView в 5 раз больше ширины буквы "M".
