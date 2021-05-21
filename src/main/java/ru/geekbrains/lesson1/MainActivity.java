981180c2b4cfe174c72bcdf974446b240c57c72a
SergeOg <gosha_006@mail.ru>
2021-05-21 17:42:06 +0300

    Реализация Task_1

--- src/main/java/ru/geekbrains/lesson1/MainActivity.java
+++ src/main/java/ru/geekbrains/lesson1/MainActivity.java
@@ -1,8 +1,11 @@
 package ru.geekbrains.lesson1;
-
-import androidx.appcompat.app.AppCompatActivity;
+//1. Создать проект со следующими пользовательскими элементами: TextView, EditText, Button,
+//Switch, CheckBox, ToggleButton. Для работы использовать LinearLayout. Использовать
+//различные шрифты, цвета, размеры, прочие атрибуты.
 
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
